433a54260a924437641d3a0cafef7cf6a5d5f3a3
Dennis Rocha <dennis0201@gmail.com>
2024-03-27 16:57:45 -0300

    Update

--- page_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from page_app.views import index, services
-
-urlpatterns = [
-    # Cadastrar URLs aqui
-    path('', index),
-    path('services', services),
-]
--- page_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index (request):
-    return render(request,"page_app/partial/home.html")
-
-def services (request):
-    return render(request, "page_app/partial/home.html")
